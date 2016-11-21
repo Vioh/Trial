33f3934102af52061125d06469140c7741b1bbe0
Vioh <syhd.ifi.uio.no>
2016-11-21 18:32:17 +0100

    lol

--- facts.pl
+++ facts.pl
@@ -1,7 +1,15 @@
-nationality(ola, norway).
-nationality(eyvind, norway).
-nationality(raji, india).
-nationality(rajesh, india).
-likes(Name, Dish) :- nationality(Name,norway), italian(Dish).
-likes(Name, Dish) :- nationality(Name,norway), indian(Dish), mild(Dish).
-likes(Name, Dish) :- nationality(Name,india), indian(Dish).
+star(sun).
+star(sirius).
+star(vega).
+orbits(mercury, sun). 
+orbits(venus, sun).
+orbits(earth, sun). 
+orbits(mars, sun). 
+orbits(moon, earth).
+orbits(deimos, mars).
+
+planet(B) :- orbits(B,sun).
+satellite(B) :- orbits(B,P), planet(P).
+solar(B) :- sun.
+solar(B) :- planet(B). 
+solar(B) :- satellite(B).

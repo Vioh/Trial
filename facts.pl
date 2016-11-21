82b1cac39e6d335bd9b130aea6e5f53a53739a3d
Vioh <syhd.ifi.uio.no>
2016-11-21 20:30:24 +0100

    lol

--- facts.pl
+++ facts.pl
@@ -1,13 +1,19 @@
-p(anne, aase, aale, 1960).
-p(arne, aase, aale, 1962).
-p(beate, anne, lars, 1989).
-p(bjorn, lise, arne, 1990).
-p(ginny, ida, hans, 1984).
+reindeerLine(northpole,oslo). 
+reindeerLine(oslo,london). 
+reindeerLine(oslo,copenhagen). 
+reindeerLine(copenhagen,berlin). 
+reindeerLine(northpole,stockholm). 
+reindeerLine(stockholm,moscow).
+reindeerLine(helsinki,moscow).
 
-child(X,Y) :- p(X,_,Y,_).
-child(X,Y) :- p(X,Y,_,_).
-gc(X,Y) :- child(X,Z), child(Z,Y).
 
-descendant(X,Y) :- child(X,Y).
-descendant(X,Y) :- child(X,Z), cd(Z,Y).
-cd(X,Y) :- X \== Y, descendant(X,D1), descendant(Y,D2), D1 == D2.
+
+
+
+line(X,Y) :- reindeerLine(X,Y).
+line(X,Y) :- reindeerLine(X,Z), line(Z,Y).
+
+
+
+
+

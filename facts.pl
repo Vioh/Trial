cb99da0a57d1c4244baebf85fbdca6261e65563f
Vioh <syhd.ifi.uio.no>
2016-11-21 18:10:35 +0100

    lol

--- facts.pl
+++ facts.pl
@@ -1,10 +1,7 @@
-italian(pizza).
-italian(spaghetti).
-indian(tandoori).
-indian(curry).
-indian(tikkaMasala).
-hot(tikkaMasala).
-mild(tandoori). 
-
-likes(ola, Dish) :- italian(Dish).
-likes(ola, Dish) :- indian(Dish), mild(Dish).
+nationality(ola, norway).
+nationality(eyvind, norway).
+nationality(raji, india).
+nationality(rajesh, india).
+likes(Name, Dish) :- nationality(Name,norway), italian(Dish).
+likes(Name, Dish) :- nationality(Name,norway), indian(Dish), mild(Dish).
+likes(Name, Dish) :- nationality(Name,india), indian(Dish).

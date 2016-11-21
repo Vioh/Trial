2404e73bb43801f10ab750831ad1828c4f97093d
Vioh <syhd.ifi.uio.no>
2016-11-21 19:27:48 +0100

    lol

--- facts.pl
+++ facts.pl
@@ -1,15 +1,13 @@
-star(sun).
-star(sirius).
-star(vega).
-orbits(mercury, sun). 
-orbits(venus, sun).
-orbits(earth, sun). 
-orbits(mars, sun). 
-orbits(moon, earth).
-orbits(deimos, mars).
+p(anne, aase, aale, 1960).
+p(arne, aase, aale, 1962).
+p(beate, anne, lars, 1989).
+p(bjorn, lise, arne, 1990).
+p(ginny, ida, hans, 1984).
 
-planet(B) :- orbits(B,sun).
-satellite(B) :- orbits(B,P), planet(P).
-solar(B) :- sun.
-solar(B) :- planet(B). 
-solar(B) :- satellite(B).
+child(X,Y) :- p(X,_,Y,_).
+child(X,Y) :- p(X,Y,_,_).
+gc(X,Y) :- child(X,Z), child(Z,Y).
+
+descendant(X,Y) :- child(X,Y).
+descendant(X,Y) :- child(X,Z), cd(Z,Y).
+cd(X,Y) :- X \== Y, descendant(X,D1), descendant(Y,D2), D1 == D2.
